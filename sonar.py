48d20669c89a054adb47a781acabacc9d4d4ee8c
javierrome236 <javierromero236@gmail.com>
2023-03-28 16:59:20 +0200

    update .jar and sonar.py

--- sonar.py
+++ sonar.py
@@ -5,14 +5,13 @@ import requests
 
 def generate():
     # Open the file and load the file
-    with open('openapi.yaml', encoding='utf8') as f:
+    with open('openapi.yaml') as f:
         data = yaml.load(f, Loader=SafeLoader)
     urls={}
     names={}
     for i in data['paths']:
         for j in data['paths'][i]:
             if 'x-quantumCode' in data['paths'][i][j]:
-                print(data['paths'][i][j])
                 urls[data['paths'][i][j]['x-quantumCode']]=data['paths'][i][j]['x-quantumProvider']
             if 'operationId' in data['paths'][i][j] and 'x-quantumCode' in data['paths'][i][j]:
                 names[data['paths'][i][j]['x-quantumCode']]=data['paths'][i][j]['operationId']
@@ -23,14 +22,13 @@ def generate():
             x = requests.get(i).text
             f.write(x)
         else:
+            header_value=i
+            header_value_utf8 = header_value.encode('utf-8')
             if urls[i]=='aws':
-                header_value=i
-                header_value_utf8 = header_value.encode('utf-8')
                 x = requests.get('http://quantumservicesdeployment.spilab.es:8081/code/aws', headers={"x-url":header_value_utf8}).json()['code']
             else:
                 x = requests.get('http://quantumservicesdeployment.spilab.es:8081/code/ibm', headers={"x-url":header_value_utf8}).json()['code']
             for line in x:
-
                 f.write(line+'\n')
         f.close()
 
