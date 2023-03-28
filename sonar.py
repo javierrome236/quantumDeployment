e811b77502d52f235bf1b93dcf2cefe05378a9da
JaimeAlvaradoValiente <jalvaradol@alumnos.unex.es>
2023-03-28 14:58:27 +0200

    sonar

--- sonar.py
+++ sonar.py
@@ -5,7 +5,7 @@ import requests
 
 def generate():
     # Open the file and load the file
-    with open('openapi.yaml', encoding='latin-1') as f:
+    with open('openapi.yaml', encoding='utf8') as f:
         data = yaml.load(f, Loader=SafeLoader)
     urls={}
     names={}
@@ -23,9 +23,11 @@ def generate():
             f.write(x)
         else:
             if urls[i]=='aws':
-                x = requests.get('http://quantumservicesdeployment.spilab.es:8081/code/aws', headers={"x-url":i}).json()['code']
+                header_value=i
+                header_value_utf8 = header_value.encode('utf-8')
+                x = requests.get('http://quantumservicesdeployment.spilab.es:8081/code/aws', headers={"x-url":header_value_utf8}).json()['code']
             else:
-                x = requests.get('http://quantumservicesdeployment.spilab.es:8081/code/ibm', headers={"x-url":i}).json()['code']
+                x = requests.get('http://quantumservicesdeployment.spilab.es:8081/code/ibm', headers={"x-url":header_value_utf8}).json()['code']
             for line in x:
 
                 f.write(line+'\n')
